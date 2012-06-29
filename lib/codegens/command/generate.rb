e5df1a6bfb4165c51265ca55e434b8da439eddf8
Anthony Crumley <anthony.crumley@gmail.com>
2012-06-29 16:00:09 -0500

    Added answer based conditions and file names.

--- lib/codegens/command/generate.rb
+++ lib/codegens/command/generate.rb
@@ -17,27 +17,31 @@ module Codegens
       def create_content(relative_template_path, relative_content_path, content)
         if File.directory?(File.join(template_path, relative_template_path, content))
           relative_content_path = create_directory(relative_content_path, content)
-          render_directory(File.join(relative_template_path, content), relative_content_path)
+          render_directory(File.join(relative_template_path, content), relative_content_path) unless relative_content_path.nil?
         else
           create_file(relative_template_path, relative_content_path, content)
         end
       end
 
       def create_directory(relative_content_path, path)
-        path = File.join(relative_content_path, interpret(path))
-        Dir.mkdir(Codegens.generation_path(path))
+        if path = interpret(path)
+          path = File.join(relative_content_path, path)
+          Dir.mkdir(Codegens.generation_path(path))
+        end
 
         return path
       end
 
       def create_file(relative_template_path, relative_content_path, file_name)
-        source_path = File.join(template_path, relative_template_path, file_name)
-        destination_path = Codegens.generation_path(File.join(relative_content_path, interpret(file_name)))
-        
-        args = self.template_args
-        answers = self.template_answers
-        File.open(destination_path, 'w') do |f|
-          f.puts(ERB.new(File.read(source_path)).result(binding))
+        if destination_file_name = interpret(file_name)
+          source_path = File.join(template_path, relative_template_path, file_name)
+          destination_path = Codegens.generation_path(File.join(relative_content_path, destination_file_name))
+          
+          args = self.template_args
+          answers = self.template_answers
+          File.open(destination_path, 'w') do |f|
+            f.puts(ERB.new(File.read(source_path)).result(binding))
+          end
         end
       end
 
@@ -46,9 +50,25 @@ module Codegens
       end
 
       def interpret(name)
+        if /(.+)_if_(.+)/.match(name)
+          name = $1
+          condition = $2
+          if /answer_(.+)/.match(condition)
+            answer = self.template_answers[$1]
+            if answer.nil? or answer.empty?
+              return nil
+            end
+          end
+        end
+
+        if /answer_(.+)/.match(name)
+          name = name.gsub("answer_#{$1}", self.template_answers[$1])
+        end
+
         while /arg(\d+)/.match(name)
           name = name.gsub("arg#{$1}", self.template_args[$1.to_i])
         end
+
         name
       end
 
--- test/generate_command.rb
+++ test/generate_command.rb
@@ -1,10 +1,41 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_setup')
 
 class TestGenerateCommand < MiniTest::Unit::TestCase
+  def test_answer_named_directory
+
+  end
+
+  def test_answer_named_file
+    refute File.exists?(generation_path('bin/exebar')), "File 'exebar' already exists."
+    Codegens::Command::Generate.new('foo', ['bar'], {"executable" => "exebar"}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'bin_if_answer_executable')
+    Codegens::Command::Generate.new('foo', ['bar'], {"executable" => "exebar"}).create_file('arg0/bin_if_answer_executable', 'bin', 'answer_executable')
+    assert File.exists?(generation_path('bin/exebar')), "File 'exebar' does not exist."
+  end
+
   def test_argument_named_directory
     refute File.exists?(generation_path('bar')), "Directory 'bar' already exists"
     Codegens::Command::Generate.new('foo', ['bar'], {}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'arg0')
     assert File.exists?(generation_path('bar')), "Directory 'bar' does not exist"
+  end
+
+  def test_argument_named_file
+    refute File.exists?(generation_path('bar/bar.gemspec')), "File 'bar.gemspec' already exists."
+    Codegens::Command::Generate.new('foo', ['bar'], {}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'arg0')
+    Codegens::Command::Generate.new('foo', ['bar'], {}).create_file('arg0', 'bar', 'arg0.gemspec')
+    assert File.exists?(generation_path('bar/bar.gemspec')), "File 'bar.gemspec' does not exist."
+  end
+
+  def test_conditional_directory
+    refute File.exists?(generation_path('created')), "Directory 'created' already exists."
+    refute File.exists?(generation_path('not_created')), "Directory 'not_created' already exists."
+    Codegens::Command::Generate.new('foo', ['bar'], {"executable" => "exebar"}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'created_if_answer_executable')
+    Codegens::Command::Generate.new('foo', ['bar'], {}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'not_created_if_answer_executable')
+    refute File.exists?(generation_path('not_created')), "Directory 'not_created' exists."
+    assert File.exists?(generation_path('created')), "Directory 'created' does not exist"
+  end
+
+  def test_conditional_file
+
   end
 
   def test_explicitly_named_directory
@@ -13,10 +44,18 @@ class TestGenerateCommand < MiniTest::Unit::TestCase
     assert File.exists?(generation_path('lib')), "Directory 'lib' does not exist"
   end
 
-  def test_static_file
+  def test_explicitly_named_file
     refute File.exists?(generation_path('bar/README.rdoc')), "File 'README.rdoc' already exists."
     Codegens::Command::Generate.new('foo', ['bar'], {}).create_directory(Codegens::Command::Generate::CURRENT_DIRECTORY, 'arg0')
     Codegens::Command::Generate.new('foo', ['bar'], {}).create_file('arg0', 'bar', 'README.rdoc')
     assert File.exists?(generation_path('bar/README.rdoc')), "File 'README.rdoc' does not exist."
   end
+
+  def test_name_interpretation
+    assert 'bar' == Codegens::Command::Generate.new('foo', ['bar'], {}).interpret('arg0'), "Name interpretation of arguments failed."
+    assert_nil Codegens::Command::Generate.new('foo', ['bar'], {}).interpret('bin_if_answer_executable'), "If modifier interpretation with answers failed."
+    assert_equal 'bin', Codegens::Command::Generate.new('foo', ['bar'], {"executable" => "exebar"}).interpret('bin_if_answer_executable'), "If modifier interpretation with answers failed."
+    assert_equal 'exebar', Codegens::Command::Generate.new('foo', ['bar'], {"executable" => "exebar"}).interpret('answer_executable'), "Name interpretation of answers failed."
+  end
+
 end
